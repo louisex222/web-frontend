3742301160e9f21a12268b13c49a7e6e04b010c6
louisex222 <louisex222@gmail.com>
2025-11-14 12:24:25 +0800

    feat: 部署跟靜態資源處理

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  base: mode === 'production' ? '/web-frontend-exam/' : '/',
+  build: {
+    outDir: 'dist',
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+}))
+
